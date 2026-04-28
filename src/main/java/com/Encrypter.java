9008e6258b39c33d28044fc9b3a725ea37141e8d
Cristina ❤️ <mim0xina@gmail.com>
2026-04-28 17:31:27 +0200

    Creación de método encryptMessage y decryptMessage

--- src/main/java/com/Encrypter.java
+++ src/main/java/com/Encrypter.java
@@ -1,8 +1,8 @@
 package com;
 
-import javax.crypto.KeyGenerator;
-import javax.crypto.SecretKey;
-import java.nio.charset.StandardCharsets;
+import javax.crypto.*;
+import java.security.InvalidKeyException;
+import java.security.NoSuchAlgorithmException;
 import java.security.SecureRandom;
 
 /**
@@ -11,15 +11,14 @@ import java.security.SecureRandom;
 public class Encrypter {
     String seed;
     String textToEncrypt;
-    String textEncrypted;
-
+    byte[] textEncrypted;
     SecretKey key;
 
     // --- Constructor ---
     public Encrypter(){
         this.seed = "";
         this.textToEncrypt = "";
-        this.textEncrypted = "";
+        this.textEncrypted = "".getBytes();
     }
 
     /**
@@ -27,16 +26,59 @@ public class Encrypter {
      * parámetro
      * En este caso, se utiliza una clave AES simétrica de 128 bits
      * @param seed semilla a utilizar
-     * @return SecretKey con la clave
      */
-    public SecretKey generateKey(String seed){
+    public void generateKey(String seed) throws NoSuchAlgorithmException {
         // SecureRandom genera claves para números aleatorios con semilla
         SecureRandom random = new SecureRandom(seed.getBytes());
 
         //KeyGenerator genera la clave AES de 128 bits
         KeyGenerator keyGen = KeyGenerator.getInstance("AES");
         keyGen.init(128, random); //128 bits + numero aleatorio con semilla
-        return keyGen.generateKey();
+        key = keyGen.generateKey(); //Almacenamos la clave
+    }
+
+
+    /**
+     * Método que cifra un mensaje mediante el cifrado AES de 128 bits (16 bytes) y lo almacena en las propiedades de
+     * la clase.
+     *
+     * @param msg mensaje a cifrar
+     * @throws NoSuchPaddingException
+     * @throws NoSuchAlgorithmException
+     * @throws InvalidKeyException
+     * @throws IllegalBlockSizeException
+     * @throws BadPaddingException
+     */
+    public void encryptMessage(String msg) throws NoSuchPaddingException, NoSuchAlgorithmException,
+            InvalidKeyException, IllegalBlockSizeException, BadPaddingException {
+        // se añade el throws en el método para manejar las excepciones posteriormente
+
+        // Se obtiene el objeto cypher con el algoritmo que se pide
+        Cipher cipher = Cipher.getInstance("AES/ECB/PKCS5Padding");
+
+        // Se inicia en modo cifrado con la clave que se ha definido
+        cipher.init(Cipher.ENCRYPT_MODE, key);
+
+        //Se cifra el mensaje
+        textEncrypted = cipher.doFinal(msg.getBytes());
+    }
+
+    /**
+     * Función que devuelve una lista de bytes con el mensaje descifrado con la clave proporcionada.
+     * @param textEncrypted texto encriptado que se desea traducir
+     * @return texto traducido
+     * @throws InvalidKeyException
+     * @throws IllegalBlockSizeException
+     * @throws BadPaddingException
+     * @throws NoSuchPaddingException
+     * @throws NoSuchAlgorithmException
+     */
+    public byte[] decryptMessage(byte[] textEncrypted) throws InvalidKeyException, IllegalBlockSizeException, BadPaddingException, NoSuchPaddingException, NoSuchAlgorithmException {
+        // Se inicia cipher en modo descifrado
+        Cipher cipher = Cipher.getInstance("AES/ECB/PKCS5Padding");
+        cipher.init(Cipher.DECRYPT_MODE, key);
+
+        return cipher.doFinal(textEncrypted);
     }
 
 
